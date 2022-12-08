acc5b1df9c5015c953578fe1180ac6e4638e653d
mostafaibrahim24 <mi.mostafaibrahim@gmail.com>
2022-12-08 17:25:25 +0200

    Add sjf(short job first) scheduler

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,9 +6,20 @@ class Process{
     private String processName;
     private Integer processArrivalTime;
     private Integer processBurstTime;
+    private Integer remainingBurstTime;
     private Integer processPriority;
     private Integer isAt;
     private Integer waitingTime;
+
+    private  Integer turnaroundTime;
+
+    public Integer getTurnaroundTime() {
+        return turnaroundTime;
+    }
+
+    public void setTurnaroundTime(Integer turnaroundTime) {
+        this.turnaroundTime = turnaroundTime;
+    }
 
     public Integer getWaitingTime() {
         return waitingTime;
@@ -22,6 +33,14 @@ class Process{
 
     public String getProcessName() {
         return processName;
+    }
+
+    public Integer getRemainingBurstTime() {
+        return remainingBurstTime;
+    }
+
+    public void setRemainingBurstTime(Integer remainingBurstTime) {
+        this.remainingBurstTime = remainingBurstTime;
     }
 
     public void setProcessName(String processName) {
@@ -64,6 +83,20 @@ class Process{
         return isDone;
     }
 
+    public Process() {
+    }
+
+    public Process(String processName, Integer processArrivalTime, Integer processBurstTime, Integer remainingBurstTime, Integer processPriority, Integer isAt, Integer waitingTime, Boolean isDone) {
+        this.processName = processName;
+        this.processArrivalTime = processArrivalTime;
+        this.processBurstTime = processBurstTime;
+        this.remainingBurstTime = remainingBurstTime;
+        this.processPriority = processPriority;
+        this.isAt = isAt;
+        this.waitingTime = waitingTime;
+        this.isDone = isDone;
+    }
+
     public void setDone(Boolean done) {
         isDone = done;
     }
@@ -75,17 +108,96 @@ class Process{
                 ", processArrivalTime=" + processArrivalTime +
                 ", processBurstTime=" + processBurstTime +
                 ", processPriority=" + processPriority +
-                ", isAt=" + isAt +
                 ", waitingTime=" + waitingTime +
-                ", isDone=" + isDone +
+                ", turnaroundTime=" + turnaroundTime +
                 '}';
     }
 }
 
 class ShortestJobFirstScheduler{
 
+    public static  Process getProcessOfMinWaitingTime(List<Process> processes,Integer minBurstValue){
+        List<Process> sameMinBurstProcesses = new ArrayList<Process>();
+        for (int i = 0; i < processes.size(); i++) {
+            if(processes.get(i).getRemainingBurstTime()==minBurstValue){
+                sameMinBurstProcesses.add(processes.get(i));
+            }
+        }
+        Util.sortAccordingToWaitingTime(sameMinBurstProcesses);
+        return  sameMinBurstProcesses.get(0);
+    }
+    public static Process getProcessOfMinBurst(List<Process> processes, Integer time){
+        //System.out.println("In getProcessOfMinBurst");
+        //for (int i = 0; i < processes.size(); i++) {
+        //    System.out.println(processes.get(i).toString());
+        //}
+        List<Process> notDoneProcesses = new ArrayList<Process>();
+        for(int i=0;i< processes.size();i++){
+            //System.out.println("IN LOOP OF MIN BURST: "+processes.get(i).toString());
+            if(processes.get(i).getDone()==true){
+                continue;
+            }
+            if(processes.get(i).getIsAt()<=time) {
+                processes.get(i).toString();
+                notDoneProcesses.add(processes.get(i));
+            }
+        }
+        Util.sortAccordingToBurstTime(notDoneProcesses);
+        if(notDoneProcesses.size()==0){return null;}
+        return  getProcessOfMinWaitingTime(notDoneProcesses,notDoneProcesses.get(0).getRemainingBurstTime());
+    }
     ShortestJobFirstScheduler(List<Process>processes){
+//        System.out.println("In constructor");
+//        for (int i = 0; i < processes.size(); i++) {
+//            System.out.println(processes.get(i).toString());
+//        }
+
         Util.sortAccordingToArrivalTime(processes);//Sort according to arrival time
+
+//       System.out.println("In constructor, after sort according to arrival time");
+//        for (int i = 0; i < processes.size(); i++) {
+//            System.out.println(processes.get(i).toString());
+//       }
+
+        Boolean executing= true;
+        Integer time =0;
+        while(executing){
+            Process p = getProcessOfMinBurst(processes,time);
+
+            Process tempP = new Process(p.getProcessName(),p.getProcessArrivalTime(),p.getProcessBurstTime(),p.getRemainingBurstTime(),p.getProcessPriority(),p.getIsAt(),p.getWaitingTime(),p.getDone());
+            tempP.setRemainingBurstTime(tempP.getRemainingBurstTime()-1);
+            processes.set(processes.indexOf(p),tempP);
+            if(processes.get(processes.indexOf(tempP)).getRemainingBurstTime()==0){
+                processes.get(processes.indexOf(tempP)).setDone(true);
+                processes.get(processes.indexOf(tempP)).setIsAt(processes.get(processes.indexOf(tempP)).getIsAt()+1);
+            }
+            for(int i=0;i<processes.size();i++){
+                if(processes.get(i).getDone()==false && processes.get(i).getIsAt()==time){
+                    processes.get(i).setIsAt(processes.get(i).getIsAt()+1);
+                }
+            }
+            time++;
+            Boolean isAllDone=true;
+            for(int i=0;i<processes.size();i++){
+                if(processes.get(i).getDone()==false){
+                    isAllDone=false;
+                }
+            }
+            if(isAllDone==true){
+                executing=false;
+            }
+        }
+        for(int i=0;i<processes.size();i++){
+            //System.out.println("Right Before Process print: "+processes.get(i).toString());
+          //processes.get(i).setWaitingTime((processes.get(i).getIsAt()-processes.get(i).getProcessBurstTime())-processes.get(i).getProcessArrivalTime());
+
+            Integer tempIsAt=processes.get(i).getIsAt();
+            Integer tempArrivalTime=processes.get(i).getProcessArrivalTime();
+            Integer tempBurstTime=processes.get(i).getProcessBurstTime();
+            processes.get(i).setWaitingTime(tempIsAt-(tempArrivalTime+tempBurstTime));
+            processes.get(i).setTurnaroundTime(tempBurstTime+processes.get(i).getWaitingTime());
+            System.out.println("Process print: "+processes.get(i).toString());
+        }
 
     }
 }
@@ -119,16 +231,16 @@ class Util{
                 }
         );
     }
-    public static void sortAccordingToBurstTime(ArrayList<Process> processes){
+    public static void sortAccordingToBurstTime(List<Process> processes){
         Collections.sort(processes, new Comparator<Process>() {
                     @Override
                     public int compare(Process p1, Process p2) {
-                        return p1.getProcessBurstTime().compareTo(p2.getProcessBurstTime());
+                        return p1.getRemainingBurstTime().compareTo(p2.getRemainingBurstTime());
                     }
                 }
         );
     }
-    public static void sortAccordingToWaitingTime(ArrayList<Process> processes){
+    public static void sortAccordingToWaitingTime(List<Process> processes){
         Collections.sort(processes, new Comparator<Process>() {
                     @Override
                     public int compare(Process p1, Process p2) {
@@ -153,24 +265,27 @@ public class Main {
 
         for(int i=0;i<numberOfProcesses;i++){
             Process process = new Process();
-            System.out.println("Process "+ (i+1) +" name: ");
+            System.out.print("Process "+ (i+1) +" name: ");
             process.setProcessName(scanner.next());
 
-            System.out.println("Process "+ (i+1) +" arrival time: ");
+            System.out.print("Process "+ (i+1) +" arrival time: ");
             process.setProcessArrivalTime(scanner.nextInt());
             process.setIsAt(process.getProcessArrivalTime());
 
-            System.out.println("Process "+ (i+1) +" burst time: ");
+            System.out.print("Process "+ (i+1) +" burst time: ");
             process.setProcessBurstTime(scanner.nextInt());
+            process.setRemainingBurstTime(process.getProcessBurstTime());
 
-            System.out.println("Process "+ (i+1) +" priority: ");
-            process.setProcessPriority(scanner.nextInt());
+            //System.out.print("Process "+ (i+1) +" priority: ");
+            //process.setProcessPriority(scanner.nextInt());
 
             process.setDone(false);
-
+            process.setWaitingTime(0);
             processes.add(process);
         }
-
-
+//        for (int i = 0; i < processes.size(); i++) {
+//            System.out.println(processes.get(i).toString());
+//        }
+        ShortestJobFirstScheduler shortestJobFirstScheduler = new ShortestJobFirstScheduler(processes);
     }
 }
